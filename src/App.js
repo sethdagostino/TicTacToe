394d82581f9d68e5f84ceba4aee2055009c29679
Seth D'Agostino <sdagosti@ucsd.edu>
2018-11-09 19:59:24 -0800

    First commit, working app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import GameBoard from './containers/GameBoard';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
-class App extends Component {
+library.add(far, faTimes);
+
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="centered-container">
+        <GameBoard />
       </div>
     );
   }
