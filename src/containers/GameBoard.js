340eba7408e2957399a089ee9c709d4ee05ac404
Seth D'Agostino <sdagosti@ucsd.edu>
2018-11-09 20:06:31 -0800

    First commit, working app

---
+++ src/containers/GameBoard.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import Cell from './Cell';
+import { changeCellNumberToSymbol, isThisAWinningMove, boardIsFull } from '../TicTacToeGridLogic'
+
+class GameBoard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newGame: true,
+            gameOver: false,
+            whichTurn: "X",
+            grid: [['', '', ''], ['', '', ''], ['', '', '']],
+            gameWon: false
+        }
+        this.handleClickBlankCell = this.handleClickBlankCell.bind(this);
+        this.handleResetClick = this.handleResetClick.bind(this);
+        this.makeMove = this.makeMove.bind(this);
+        this.switchTurns = this.switchTurns.bind(this);
+    }
+
+    makeMove(symbol, cellNumber) {
+        console.log(`Setting cell ${cellNumber} to a ${symbol}`);
+        let revisedGrid = this.state.grid;
+        changeCellNumberToSymbol(revisedGrid, cellNumber, symbol);
+        this.setState({grid: revisedGrid})
+    }
+
+    switchTurns() {
+        if(this.state.whichTurn === "X") {
+            this.setState({whichTurn: "O"});
+        }
+        else {
+            this.setState({whichTurn: "X"});
+        }
+    }
+
+    handleClickBlankCell(cellNumber) {
+        //Blank cells should only react if the game is not over
+        if(!this.state.gameOver) {
+            //Game has started, set newGame to false
+            if(this.state.newGame) {
+                console.log("New game started!");
+                this.setState({newGame: false});
+            }
+            //Update the grid for this move and check if its a winning move
+            this.makeMove(this.state.whichTurn, cellNumber);
+            if(isThisAWinningMove(this.state.grid, cellNumber, this.state.whichTurn)) {
+                var winningSymbol = this.state.whichTurn;
+                console.log(winningSymbol + " just won!");
+                this.setState({gameOver: true, gameWon: true});
+                return;
+            }
+            //If the board is full, finish the game. Otherwise, switch turns
+            if(boardIsFull(this.state.grid)) {
+                console.log("Board is full. Game over");
+                this.setState({gameOver: true});
+            }
+            else {
+                this.switchTurns();
+            }
+        }
+    }
+
+    handleResetClick() {
+        console.log("Resetting!");
+        this.setState({
+            newGame: true,
+            gameOver: false,
+            whichTurn: "X",
+            grid: [['', '', ''], ['', '', ''], ['', '', '']],
+            gameWon: false
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <div className="centered-container">
+            <Banner newGame={this.state.newGame} gameOver={this.state.gameOver} gameWon={this.state.gameWon} whichTurn={this.state.whichTurn}/>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td><Cell symbol={this.state.grid[0][0]} onClick={() => this.handleClickBlankCell(0)}/></td>
+                        <td><Cell symbol={this.state.grid[0][1]} onClick={() => this.handleClickBlankCell(1)}/></td>
+                        <td><Cell symbol={this.state.grid[0][2]} onClick={() => this.handleClickBlankCell(2)}/></td>
+                    </tr>
+                    <tr>
+                        <td><Cell symbol={this.state.grid[1][0]} onClick={() => this.handleClickBlankCell(3)}/></td>
+                        <td><Cell symbol={this.state.grid[1][1]} onClick={() => this.handleClickBlankCell(4)}/></td>
+                        <td><Cell symbol={this.state.grid[1][2]} onClick={() => this.handleClickBlankCell(5)}/></td>
+                    </tr>
+                    <tr>
+                        <td><Cell symbol={this.state.grid[2][0]} onClick={() => this.handleClickBlankCell(6)}/></td>
+                        <td><Cell symbol={this.state.grid[2][1]} onClick={() => this.handleClickBlankCell(7)}/></td>
+                        <td><Cell symbol={this.state.grid[2][2]} onClick={() => this.handleClickBlankCell(8)}/></td>
+                    </tr>
+                </tbody>
+            </table>
+            <button id="resetButton" onClick={this.handleResetClick}>RESET</button>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default GameBoard;
---
+++ src/styles/Styles.js
@@ -0,0 +1,28 @@
+export const symbolStyle = {
+    display: "block",
+    width: "80%",
+    height: "auto",
+    margin: "auto auto",
+    position: "absolute",
+    left: "10%",
+    bottom: "10%",
+    top: "10%"
+}
+
+export const blankCell = {
+    position: "absolute",
+    cursor: "pointer",
+    top: "0",
+    bottom: "0",
+    right: "0",
+    left: "0"
+}
+
+export const bannerStyle = {
+    color: "#0099ff",
+    width: "125%",
+    borderRadius: 8,
+    border: "2px solid #0099ff",
+    marginTop: 20,
+    marginBottom: 20
+}
